08d7bee4d7c623c6ece4a9b8936adb8a2be3d2ee
leandson <leandson.ageedu@gmail.com>
2022-02-21 17:03:28 -0300

    Implemented button and redirection

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+  colors:{
+    background: "#A2CCD6",
+    primary: '#EF8200',
+    heading: '#DDE3F0',
+    line: '#000000'
+  }
+}
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,41 @@
-import React, {useState} from "react";
-import { View, Text, Image } from 'react-native';
+import React, { useCallback } from "react";
+import { View, Text, Image, StatusBar, Linking, Alert } from 'react-native';
 
 import IllustrationImg from '../../assets/illustration.png'
 import { styles } from "./styles";
+import { ButtonIcon } from "../../components/Buttonicon";
 
 export function SignIn(){
+  const eaducURL = "https://moodle.youeduc.com.br/moodle/login/index.php";
+  const handlePress = useCallback(async () => {
+    
+  const supported = await Linking.canOpenURL(eaducURL);
+
+  if (supported) {
+     await Linking.openURL(eaducURL);
+  } else {
+    Alert.alert(`Não foi possivel acessar: ${eaducURL}`);
+  }
+},[eaducURL]);
+
   return(
     <View style={styles.container}>
+      <StatusBar
+        barStyle = "light-content"
+        backgroundColor = "transparent" 
+        translucent
+      />
       <Image 
         source = {IllustrationImg}
-        style = {styles.image}/>
+        style = {styles.image}
+        resizeMode = "stretch"
+      />
+      <View style = {styles.content}>
+        <ButtonIcon title = "Acessar Plataforma"
+        activeOpacity = {0.3}
+        onPress = {handlePress}
+        />
+      </View>
     </View>
   )
 }
